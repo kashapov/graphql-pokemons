a934e51e2ed31aa8ddbcb03f63a34981d800f46a
Andrey Kashapov <kashapovandrey@gmail.com>
2019-11-02 10:40:09 +0200

    connect Apollo Client to React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo-hooks';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://graphql-pokemon.now.sh/',
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
